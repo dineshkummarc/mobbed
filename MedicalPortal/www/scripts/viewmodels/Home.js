1950a9f9da70862cc29c41482e8b3ffaffec60f9
stevegreatrex <greatrex696@hotmail.com>
2012-02-28 22:12:52 +0000

    Added jquery and knockout references
    Added Home view model
    Added ridiculously trivial view model

---
+++ MedicalPortal/www/scripts/viewmodels/Home.js
@@ -0,0 +1,9 @@
+﻿var HomeViewModel = function () {
+    this.welcomeMessage = ko.observable("Hello who?");
+    this.isUpdateLinkVisible = ko.observable(true);
+
+    this.updateMessage = function () {
+        this.welcomeMessage("Hello world!");
+        this.isUpdateLinkVisible(false);
+    };
+};
