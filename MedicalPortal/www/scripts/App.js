cf2f3dad91a0a80432cb6d9d63b540d9d29706b4
stevegreatrex <greatrex696@hotmail.com>
2012-02-28 22:34:56 +0000

    Added jquery mobile config

---
+++ MedicalPortal/www/scripts/App.js
@@ -0,0 +1,6 @@
+﻿var App = window.App = {};
+App.ViewModels = {};
+
+$(document).on("pagecreate", function(event, ui) {
+   ko.applyBindings(new App.ViewModels.HomeViewModel()); 
+});
--- MedicalPortal/www/scripts/viewmodels/Home.js
+++ MedicalPortal/www/scripts/viewmodels/Home.js
@@ -1,10 +1,12 @@
-﻿var HomeViewModel = function () {
-    this.welcomeMessage = ko.observable("Hello who?");
-    this.isUpdateLinkVisible = ko.observable(true);
+﻿(function (App) {
+    App.ViewModels.HomeViewModel = function () {
+        this.welcomeMessage = ko.observable("Hello who?");
+        this.isUpdateLinkVisible = ko.observable(true);
 
-    this.updateMessage = function () {
-        this.welcomeMessage("Hello world!");
-        this.isUpdateLinkVisible(false);
-        console.log("executed");
+        this.updateMessage = function () {
+            this.welcomeMessage("Hello world!");
+            this.isUpdateLinkVisible(false);
+            console.log("executed");
+        };
     };
-};
+})(App);
